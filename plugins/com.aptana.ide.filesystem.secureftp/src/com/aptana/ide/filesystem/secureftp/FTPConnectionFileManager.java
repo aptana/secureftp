b3397befe5a48cbdee5a42bfed30727b9bf3627e
Max Stepanov <mstepanov@aptana.com>
2010-02-04 11:26:58 -0800

    [441-ftp-read-only] set default permissions for WINDOWS type FTP servers

--- plugins/com.aptana.ide.filesystem.secureftp/src/com/aptana/ide/filesystem/secureftp/FTPConnectionFileManager.java
+++ plugins/com.aptana.ide.filesystem.secureftp/src/com/aptana/ide/filesystem/secureftp/FTPConnectionFileManager.java
@@ -97,7 +97,9 @@ import com.enterprisedt.net.ftp.pro.ProFTPClient;
 /* package */ class FTPConnectionFileManager extends BaseFTPConnectionFileManager implements IFTPConnectionFileManager {
 	
 	private static final String TMP_TIMEZONE_CHECK = ".tmp_tz_check"; //$NON-NLS-1$
-	
+
+	private final static String WINDOWS_STR = "WINDOWS";
+
 	protected FTPClient ftpClient;
 	private List<String> serverFeatures;
 	protected String transferType;
@@ -1024,8 +1026,8 @@ import com.enterprisedt.net.ftp.pro.ProFTPClient;
 	}
 
 	private FTPFile[] listFiles(IPath dirPath, IProgressMonitor monitor) throws IOException, ParseException, FTPException {
+		FTPFile[] ftpFiles = null;
 		if (statSupported != Boolean.FALSE) {
-			FTPFile[] ftpFiles = null;
 			try {
 				ftpFiles = ftpSTAT(dirPath.addTrailingSeparator().toPortableString());
 			} catch (FTPException e) {
@@ -1043,10 +1045,17 @@ import com.enterprisedt.net.ftp.pro.ProFTPClient;
 			} else if (statSupported == null) {
 				statSupported = Boolean.TRUE;
 			}
-			return ftpFiles;
 		} else {
-			return ftpLIST(dirPath, monitor);
+			ftpFiles = ftpLIST(dirPath, monitor);
 		}
+		if (fileFactory.getSystem().toUpperCase().startsWith(WINDOWS_STR) && ftpFiles != null) {
+			for (FTPFile ftpFile : ftpFiles) {
+				if (ftpFile.getPermissions() == null) {
+					ftpFile.setPermissions("-rw-r-----");
+				}
+			}
+		}
+		return ftpFiles;
 	}
 
 	private void recursiveDeleteTree(IPath path, IProgressMonitor monitor, MultiStatus status) throws IOException, ParseException {
